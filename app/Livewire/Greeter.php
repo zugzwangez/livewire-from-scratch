e95f42ab91f3bcf5ddd4712839bfd95f2d7ce9ba
Zug <zugzwangez@gmail.com>
2024-10-08 16:33:14 +0200

    Validation using Livewire attributes

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -4,14 +4,40 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+use Livewire\Attributes\Validate;
+
 class Greeter extends Component
 {
     // We can only use public properties in the View
-    public $name = 'John';
+    
+    // Validate using Livewire, validate but not stop the execution
+    #[Validate('required|min:2')]
+    public $name = '';
+    public $greeting = '';
+    public $greetingMessage = '';
 
-    public function changeName($newName) {
-        $this->name = $newName;
+    public function changeGreeting()
+    {
+        // To initialize, same as use $this->greetingMessage = ''
+        $this->reset('greetingMessage');
+
+        // validate using method rules
+        $this->validate();
+
+        // One way to validate, specify the rules direct in the validate method
+        /* $this->validate([
+            'name' => 'required|min:2',
+        ]); */
+
+        $this->greetingMessage = "{$this->greeting}, {$this->name}!";
     }
+
+    // Another way to validate
+    /* public function rules() {
+        return [
+            'name' => 'required|min:2',
+        ];
+    } */
 
     public function render()
     {
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,31 +1,38 @@
 <div>
 
-<div>
-    Mire usted, {{$name}}
-</div>
+    <form wire:submit="changeGreeting()">
 
-<form 
-    wire:submit="changeName(document.querySelector('#newName').value)"
->
-    <div class="mt-2">
+        <div class="mt-2">
 
+            <select type="text" class="p-4 border rounded-lg bg-gray-300 text-black" wire:model.fill="greeting">
+                <option value="Mire Usted">Mire usted</option>
+                <option value="Sisisisi">Sisisisisi</option>
+                <option value="Es el alcalde" selected>Es el alcalde</option>
+                <option value="Un plato es un plato">Un plato es un plato</option>
+            </select>
 
-        <input 
-            id="newName"
-            type="text" 
-            class="block w-full p-4 my-4 border rounded-lg"
-        >
+            <input id="newName" type="text" class="block w-full p-4 my-4 border rounded-lg"
+                wire:model.change="name">
 
-        <button
-            type="submit"
-            class="text-red-400 font-lg rounded-lg p-4 bg-zinc-200"
-        >
-            Greetings
-        </button>
+            @error('name')
+                <div class="my-2 bg-red-400">
+                    {{$message}}
+                </div>    
+            @enderror
 
+            <button type="submit" class="text-black font-lg rounded-lg p-4 bg-zinc-200">
+                Greetings
+            </button>
 
-    </div>
+        </div>
 
-</form>
+    </form>
+
+    @if ($greetingMessage != '')
+        <div class="bg-yellow-500 text-black p-2 mt-4">
+            {{ $greetingMessage }}
+        </div>
+    @endif
+
 
 </div>
