d8ec3291a535c4be8971da56826e73005d9abace
Zug <zugzwangez@gmail.com>
2024-10-08 15:37:40 +0200

    Actions Form wire submit

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -9,6 +9,10 @@ class Greeter extends Component
     // We can only use public properties in the View
     public $name = 'John';
 
+    public function changeName($newName) {
+        $this->name = $newName;
+    }
+
     public function render()
     {
         return view('livewire.greeter');
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,3 +1,31 @@
 <div>
-    Mire usted {{$name}}
+
+<div>
+    Mire usted, {{$name}}
+</div>
+
+<form 
+    wire:submit="changeName(document.querySelector('#newName').value)"
+>
+    <div class="mt-2">
+
+
+        <input 
+            id="newName"
+            type="text" 
+            class="block w-full p-4 my-4 border rounded-lg"
+        >
+
+        <button
+            type="submit"
+            class="text-red-400 font-lg rounded-lg p-4 bg-zinc-200"
+        >
+            Greetings
+        </button>
+
+
+    </div>
+
+</form>
+
 </div>
